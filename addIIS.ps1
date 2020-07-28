ff29feb496672b1860d0431df10c3da8786a4b96
Camiu Chen <34721863+qqnarwhal@users.noreply.github.com>
2020-07-28 18:48:50 -0500

    Add files via upload

---
+++ addIIS.ps1
@@ -0,0 +1,31 @@
+﻿# This script adds a CSE that enables IIS server role on an existing VMSS and create 
+
+# Define the variables
+$RG = "MyRG" 
+$vmssName = "MyVmss"
+
+# Define the script for your Custom Script Extension to run
+$publicSettings = @{
+    "fileUris" = (,"https://raw.githubusercontent.com/Azure-Samples/compute-automation-configurations/master/automate-iis.ps1");
+    "commandToExecute" = "powershell -ExecutionPolicy Unrestricted -File automate-iis.ps1"
+}
+
+# Get information about the scale set
+$vmss = Get-AzVmss `
+  -ResourceGroupName $RG `
+  -VMScaleSetName $vmssName
+
+# Use Custom Script Extension to install IIS and configure basic website
+Add-AzVmssExtension -VirtualMachineScaleSet $vmss `
+  -Name "customScript" `
+  -Publisher "Microsoft.Compute" `
+  -Type "CustomScriptExtension" `
+  -TypeHandlerVersion 1.8 `
+  -Setting $publicSettings
+
+# Update the scale set and apply the Custom Script Extension to the VM instances
+Update-AzVmss `
+  -ResourceGroupName $RG `
+  -Name $vmssName `
+  -VirtualMachineScaleSet $vmss
+
---
+++ buildManualVmss.ps1
@@ -0,0 +1,66 @@
+﻿# This script creates a VMSS with Manual Upgrade Policy behind a LB, and an NSG rule that opens port 80
+
+# Define the variables
+$RG = "MyRG" # New RG will be created if it doesn't exist 
+$vmssName = "MyVmss"
+$Location = "CentralUS"
+$myVnet = "MyVnet" # New VNET will be created if it doesn't exist 
+$subnet = "default" # New subnet will be created if it doesn't exist 
+
+# No need to modify this part
+$PIP = ($vmssName + "-PIP")
+$LB = ($vmssName + "-LB")
+
+# Create VMSS
+New-AzVmss `
+  -ResourceGroupName $RG `
+  -Location $Location `
+  -VMScaleSetName $vmssName `
+  -VirtualNetworkName $myVNet `
+  -SubnetName $subnet `
+  -PublicIpAddressName $PIP `
+  -LoadBalancerName $LB `
+  -UpgradePolicyMode "Automatic"
+
+
+# Get information about the scale set
+$vmss = Get-AzVmss `
+  -ResourceGroupName $RG `
+  -VMScaleSetName $vmssName
+#Create a rule to allow traffic over port 80
+$nsgFrontendRule1 = New-AzNetworkSecurityRuleConfig `
+  -Name Port_80 `
+  -Protocol Tcp `
+  -Direction Inbound `
+  -Priority 200 `
+  -SourceAddressPrefix * `
+  -SourcePortRange * `
+  -DestinationAddressPrefix * `
+  -DestinationPortRange 80 `
+  -Access Allow
+
+#Create a network security group and associate it with the rule
+$nsgFrontend = New-AzNetworkSecurityGroup `
+  -ResourceGroupName  $RG `
+  -Location $Location `
+  -Name ($vmssName + "-NSG") `
+  -SecurityRules $nsgFrontendRule1
+
+$vnet = Get-AzVirtualNetwork `
+  -ResourceGroupName  $RG `
+  -Name $myVnet
+
+$frontendSubnet = $vnet.Subnets[0]
+
+$frontendSubnetConfig = Set-AzVirtualNetworkSubnetConfig `
+  -VirtualNetwork $vnet `
+  -Name $subnet `
+  -AddressPrefix $frontendSubnet.AddressPrefix `
+  -NetworkSecurityGroup $nsgFrontend
+
+Set-AzVirtualNetwork -VirtualNetwork $vnet
+
+# Get LB IP for VMSS
+Get-AzPublicIPAddress `
+  -ResourceGroupName $RG `
+  -Name $PIP | select IpAddress
